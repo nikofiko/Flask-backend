6fcdca81811f3eb64b3330426c9f9c56b415fc7a
Adrian Flak <nikodemwilk69@gmail.com>
2023-11-24 11:00:37 +0100

    adding more

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from bson import ObjectId
 from flask import Flask, render_template, request, jsonify, json
-from database import db
+from database import db, weather
 from utils.show_json import show_json
 from flask_cors import CORS
+import requests, time
+import threading
+from werkzeug.security import generate_password_hash, check_password_hash
+import re
+from utils.regex import password_regex, email_regex
 
 app = Flask(__name__)
 
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route("/create-travel", methods=["GET", "POST"])
 def create_travel():
@@ -77,3 +83,106 @@ def delete_travel(id):
     except Exception as e:
         print(str(e))
         return show_json("Nie udało się usunąć wycieczki", 500, False)
+
+def napraw_temp(x):
+    x = round(x - 273.15, 2)
+    return x
+
+def weather_data():
+    print('Pobieram dane')
+    response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=912c622485ebcccfe6e75ebb3dc2de10')
+    data = response.json()
+    db.weather.insert_one({
+        "temp": napraw_temp(data['main']['temp']),
+        "min_temp": napraw_temp(data['main']['temp_min']),
+        "max_temp": napraw_temp(data['main']['temp_max']),
+        "feels_like": napraw_temp(data['main']['feels_like']),
+        "humidity": data['main']['humidity'],
+        "pressure": data['main']['pressure'],
+        "description": data['weather'][0]['description'],
+        "time":time.strftime("%H-%M"),
+        "date":time.strftime("%d-%m-%Y"),
+        "city": data['name']
+    })
+
+@app.route("/show-weather")
+def show_weather():
+     data = db.weather.find({})
+     weather = []
+     for item in data:
+        item['_id'] = str(item['_id'])
+        weather.append(item)
+
+     return show_json("Udało się pobrać dane",200,True,weather) 
+
+#register
+@app.route("/register", methods=["POST"])
+def register():
+    username = request.json['username']
+    email = request.json['email']
+    password = request.json['password']
+    hashed_password = generate_password_hash(password)
+
+    if db.users.find_one({"username":username}):
+        return show_json("Użytkownik o podanej nazwie już istnieje", 400, False)
+    
+    if db.users.find_one({"email": email}):
+        return show_json("Email został już użyty", 400, False)
+
+    if re.match(password_regex, password) is None:
+        return show_json("Hasło musi zawierać mała, dużą literę, cyfrę, minimum 8 znaków i znak specjalny", 400, False)
+    
+    if re.match(email_regex, email) is None:
+        return show_json("Podaj poprawny adres email", 400, False)
+
+    new_user = {
+        "username": username,
+        "email": email,
+        "password": hashed_password
+    }
+
+    db.users.insert_one(new_user)
+    new_user['_id'] = str(new_user['_id'])
+
+    return show_json("Utworzono konto", 201, True, new_user)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#schedule.every(5).minutes.do(weather_data)
+
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
+
+# def download_weather_data():
+#     weather_data()
+#     threading.Timer(600.0, download_weather_data).start()
+
+# download_weather_data()
+
+# @app.route("/data/<id>", methods=["GET", "POST"])
+# def data(id):
+
--- database.py
+++ database.py
@@ -2,5 +2,6 @@ from pymongo import MongoClient
 
 connection = MongoClient("mongodb://localhost:27017")
 db = connection["rainbow"]
+weather = db["weather"]
 
 
